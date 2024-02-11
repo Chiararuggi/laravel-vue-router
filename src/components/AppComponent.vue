fe4936fa9b56e99fce5bfd5436d03c7196c762e7
carlo alberto mallarino <carlo.alberto.mallarino@gmail.com>
2024-02-11 17:03:57 +0100

    fixed col style

--- src/components/AppComponent.vue
+++ src/components/AppComponent.vue
@@ -22,12 +22,16 @@ export default {
 </script>
 
 <template>
-    <div class="col-md-4 gy-4">
-        <div class="card h-100" v-for="event in store.eventList">
-            <div class="card-body">
-                <h5 class="card-title">{{ event.name }}</h5>
-                <p class="card-text">Event date: {{ event.date }}</p>
-                <p class="card-text">Avalaible tickets: {{ event.available_tickets }}</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 gy-4" v-for="event in store.eventList">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ event.name }}</h5>
+                        <p class="card-text">Event date: {{ event.date }}</p>
+                        <p class="card-text">Avalaible tickets: {{ event.available_tickets }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
